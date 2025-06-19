d9fffae5fa5ec16c15d619710763ea59465c943b
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-06-18 21:36:40 -0300

    initial commit

---
+++ backend/MyRepos/MyRepos.Api/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyRepos.Application;
+using MyRepos.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
